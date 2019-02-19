1ea2f3197d5dba528fac0a4b498e57f8144c8eee
David Martínez <david.martinez@aplicode.com>
2019-02-19 13:22:04 +0100

    Creacion clases Hollidays

---
+++ src/Modules/HR/Utils/HRHollidaysUtils.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Modules\HR\Utils;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use App\Modules\Globale\Entity\MenuOptions;
+use App\Modules\Email\Entity\EmailAccounts;
+use App\Modules\Globale\Utils\FormUtils;
+use App\Modules\Globale\Utils\ListUtils;
+
+class HRHollidaysUtils extends Controller
+{
+
+
+
+
+}
