45f9137e333af1d8b633c3074e1c3dd559085ab7
David Martínez <david.martinez@aplicode.com>
2019-03-09 09:36:08 +0100

    OpenLayers created

--- src/Modules/Tracker/Controller/TrackerTrackersController.php
+++ src/Modules/Tracker/Controller/TrackerTrackersController.php
@@ -56,6 +56,31 @@ class TrackerTrackersController extends Controller
   }
 
   /**
+   * @Route("/{_locale}/trackers/tracker/locations", name="trackersLocations")
+   */
+  public function trackersLocations(RouterInterface $router,Request $request)
+  {
+  $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+  //$this->denyAccessUnlessGranted('ROLE_ADMIN');
+  $userdata=$this->getUser()->getTemplateData();
+  $locale = $request->getLocale();
+  $this->router = $router;
+  $menurepository=$this->getDoctrine()->getRepository(GlobaleMenuOptions::class);
+
+  if ($this->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+    return $this->render('@Tracker/locations.html.twig', [
+      'controllerName' => 'trackersController',
+      'interfaceName' => 'Trackers',
+      'optionSelected' => 'trackers',
+      'menuOptions' =>  $menurepository->formatOptions($userdata["roles"]),
+      'breadcrumb' =>  'trackers',
+      'userData' => $userdata
+      ]);
+  }
+  return new RedirectResponse($this->router->generate('app_login'));
+  }
+
+  /**
    * @Route("/{_locale}/trackers/data/{id}/{action}", name="dataTrackers", defaults={"id"=0, "action"="read"})
    */
    public function data($id, $action, Request $request){
