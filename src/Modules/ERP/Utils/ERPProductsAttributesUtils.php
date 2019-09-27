b94808cea448d195e53e3abff39afe3a04cd6b98
Oscar Soto <oscar.soto@ferricam.com>
2019-09-27 17:39:08 +0200

    Updated Attributes

--- src/Modules/ERP/Utils/ERPProductsAttributesUtils.php
+++ src/Modules/ERP/Utils/ERPProductsAttributesUtils.php
@@ -6,19 +6,22 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use App\Modules\Globale\Entity\GlobaleMenuOptions;
-
+use App\Modules\ERP\Entity\ERPProducts;
 class ERPProductsAttributesUtils
 {
   private $module="ERP";
   private $name="ProductsAttributes";
+  public $parentClass="\App\Modules\ERP\Entity\ERPProducts";
+  public $parentField="product";
   public function getExcludedForm($params){
-    return [];
+    return ["product"];
   }
 
   public function getIncludedForm($params){
     $doctrine=$params["doctrine"];
-    $id=$params["id"];
     $user=$params["user"];
+    $product=$params["parent"];
+    $productsRepository=$doctrine->getRepository(ERPProducts::class);
     return [];
   }
 
@@ -31,6 +34,23 @@ class ERPProductsAttributesUtils
       'orderColumn' => 2,
       'orderDirection' => 'ASC',
       'tagColumn' => 2,
+      'fields' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/".$this->name.".json"),true),
+      'fieldButtons' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/".$this->name."FieldButtons.json"),true),
+      'topButtons' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/".$this->name."TopButtons.json"),true)
+    ];
+    return $list;
+  }
+
+  public function formatListByProduct($product){
+    $list=[
+      'id' => 'list'.$this->name,
+      'route' => 'genericlist',
+      'routeParams' => ["module" => $this->module,
+                        "name" => $this->name,
+                        "id" => $product],
+      'orderColumn' => 2,
+      'orderDirection' => 'ASC',
+      'tagColumn' => 3,
       'fields' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/".$this->name.".json"),true),
       'fieldButtons' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/".$this->name."FieldButtons.json"),true),
       'topButtons' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/".$this->name."TopButtons.json"),true)
