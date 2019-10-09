0fa9053eeea515e5b531e9e27142747ae5e6f48d
Oscar Soto <oscar.soto@ferricam.com>
2019-10-09 18:02:09 +0200

    Fixed states in Suppliers and Customers

--- src/Modules/ERP/Entity/ERPSuppliers.php
+++ src/Modules/ERP/Entity/ERPSuppliers.php
@@ -8,6 +8,7 @@ use \App\Modules\Globale\Entity\GlobaleActivities;
 use \App\Modules\Globale\Entity\GlobaleCurrencies;
 use \App\Modules\Globale\Entity\GlobaleCompanies;
 use \App\Modules\ERP\Entity\ERPPaymentMethods;
+use \App\Modules\Globale\Entity\GlobaleStates;
 
 /**
  * @ORM\Entity(repositoryClass="App\Modules\ERP\Repository\ERPSuppliersRepository")
@@ -51,11 +52,6 @@ class ERPSuppliers
      * @ORM\JoinColumn(nullable=false)
      */
     private $country;
-
-    /**
-     * @ORM\Column(type="string", length=125)
-     */
-    private $state;
 
     /**
      * @ORM\Column(type="string", length=12)
@@ -182,6 +178,12 @@ class ERPSuppliers
     public $newSeconds=1296000;
     public $updatedSeconds=1296000;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="\App\Modules\Globale\Entity\GlobaleStates")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $state;
+
 
     public function getId(): ?int
     {
@@ -256,18 +258,6 @@ class ERPSuppliers
     public function setCountry(?GlobaleCountries $country): self
     {
         $this->country = $country;
-
-        return $this;
-    }
-
-    public function getState(): ?string
-    {
-        return $this->state;
-    }
-
-    public function setState(string $state): self
-    {
-        $this->state = $state;
 
         return $this;
     }
@@ -556,6 +546,18 @@ class ERPSuppliers
     public function setInvoiceday(?int $invoiceday): self
     {
         $this->invoiceday = $invoiceday;
+
+        return $this;
+    }
+
+    public function getState(): ?GlobaleStates
+    {
+        return $this->state;
+    }
+
+    public function setState(?GlobaleStates $state): self
+    {
+        $this->state = $state;
 
         return $this;
     }
