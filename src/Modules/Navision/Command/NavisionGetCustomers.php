834f04648d906d26f297f3d7234e29efacf7d05e
David Martinez <david.martinez@aplicode.com>
2020-06-09 12:29:02 +0200

    Added mutex to Navision Commands

--- src/Modules/Navision/Command/NavisionGetCustomers.php
+++ src/Modules/Navision/Command/NavisionGetCustomers.php
@@ -77,6 +77,14 @@ class NavisionGetCustomers extends ContainerAwareCommand
   }
 
    public function importCustomer(InputInterface $input, OutputInterface $output){
+     //------   Create Lock Mutex    ------
+     $fp = fopen('/tmp/axiom-navisionGetCustomers-importCustomer.lock', 'c');
+     if (!flock($fp, LOCK_EX | LOCK_NB)) {
+       $output->writeln('* Fallo al iniciar la sincronizacion de clientes: El proceso ya esta en ejecución.');
+       exit;
+     }
+
+     //------   Critical Section START   ------
      $navisionSyncRepository=$this->doctrine->getRepository(NavisionSync::class);
      $navisionSync=$navisionSyncRepository->findOneBy(["entity"=>"customers"]);
      if ($navisionSync==null) {
@@ -331,11 +339,22 @@ class NavisionGetCustomers extends ContainerAwareCommand
      $navisionSync->setMaxtimestamp($objects["maxtimestamp"]);
      $this->doctrine->getManager()->persist($navisionSync);
      $this->doctrine->getManager()->flush();
+     //------   Critical Section END   ------
+     //------   Remove Lock Mutex    ------
+     fclose($fp);
    }
 
 
 
    public function importCustomerComment(InputInterface $input, OutputInterface $output){
+     //------   Create Lock Mutex    ------
+     $fp = fopen('/tmp/axiom-navisionGetCustomers-importCustomerComment.lock', 'c');
+     if (!flock($fp, LOCK_EX | LOCK_NB)) {
+       $output->writeln('* Fallo al iniciar la sincronizacion de comentarios de clientes: El proceso ya esta en ejecución.');
+       exit;
+     }
+
+     //------   Critical Section START   ------
      $navisionSyncRepository=$this->doctrine->getRepository(NavisionSync::class);
      $navisionSync=$navisionSyncRepository->findOneBy(["entity"=>"customercomments"]);
      if ($navisionSync==null) {
@@ -388,10 +407,21 @@ class NavisionGetCustomers extends ContainerAwareCommand
      $navisionSync->setMaxtimestamp($objects["maxtimestamp"]);
      $this->doctrine->getManager()->persist($navisionSync);
      $this->doctrine->getManager()->flush();
+     //------   Critical Section END   ------
+     //------   Remove Lock Mutex    ------
+     fclose($fp);
    }
 
 
    public function importCustomerContact(InputInterface $input, OutputInterface $output){
+     //------   Create Lock Mutex    ------
+     $fp = fopen('/tmp/axiom-navisionGetCustomers-importCustomerContact.lock', 'c');
+     if (!flock($fp, LOCK_EX | LOCK_NB)) {
+       $output->writeln('* Fallo al iniciar la sincronizacion de contactos de clientes: El proceso ya esta en ejecución.');
+       exit;
+     }
+
+     //------   Critical Section START   ------
      $repositoryCountries=$this->doctrine->getRepository(GlobaleCountries::class);
      $repositoryStates=$this->doctrine->getRepository(GlobaleStates::class);
      $repositoryCustomers=$this->doctrine->getRepository(ERPCustomers::class);
@@ -465,9 +495,20 @@ class NavisionGetCustomers extends ContainerAwareCommand
    $navisionSync->setMaxtimestamp($objects["maxtimestamp"]);
    $this->doctrine->getManager()->persist($navisionSync);
    $this->doctrine->getManager()->flush();
+   //------   Critical Section END   ------
+   //------   Remove Lock Mutex    ------
+   fclose($fp);
  }
 
  public function importCustomerAddresses(InputInterface $input, OutputInterface $output){
+   //------   Create Lock Mutex    ------
+   $fp = fopen('/tmp/axiom-navisionGetCustomers-importCustomerAddresses.lock', 'c');
+   if (!flock($fp, LOCK_EX | LOCK_NB)) {
+     $output->writeln('* Fallo al iniciar la sincronizacion de direcciones de clientes: El proceso ya esta en ejecución.');
+     exit;
+   }
+
+   //------   Critical Section START   ------
    $repositoryCountries=$this->doctrine->getRepository(GlobaleCountries::class);
    $repositoryStates=$this->doctrine->getRepository(GlobaleStates::class);
    $repositoryCustomers=$this->doctrine->getRepository(ERPCustomers::class);
@@ -543,6 +584,9 @@ class NavisionGetCustomers extends ContainerAwareCommand
  $navisionSync->setMaxtimestamp($objects["maxtimestamp"]);
  $this->doctrine->getManager()->persist($navisionSync);
  $this->doctrine->getManager()->flush();
+ //------   Critical Section END   ------
+ //------   Remove Lock Mutex    ------
+ fclose($fp);
 }
 
 
