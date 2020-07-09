920e886909330ea5fde672f025344e735b8ea830
Juanjo Roncero <juanjo.roncero@ferricam.com>
2020-07-09 08:03:47 +0200

    fixed CustomerCommentLinesUtils

--- src/Modules/ERP/Utils/ERPCustomerCommentLinesUtils.php
+++ src/Modules/ERP/Utils/ERPCustomerCommentLinesUtils.php
@@ -5,7 +5,11 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use App\Modules\Globale\Entity\GlobaleMenuOptions;
+use App\Modules\ERP\Entity\ERPCustomers;
+use App\Modules\ERP\Entity\ERPCustomerCommentLines;
 
 class ERPCustomerCommentLinesUtils
 {
@@ -38,20 +42,9 @@ class ERPCustomerCommentLinesUtils
 
   public function formatListByCustomerType($customer,$type){;
     $list=[
-      'id' => 'list'.$this->name,
-      'type' => $type,
+      'id' => 'list'.$this->name.$type,
       'route' => 'customercommentlineslist',
       'routeParams' => ["customerid" => $customer, "type"=>$type],
-      /*
-      'routeParams' => ["module" => $this->module,
-                        "name" => $this->name,
-                        "parent" => $supplier,
-                        'type' => $type,
-                        "field" => "supplier",
-                        "parentModule" => "ERP",
-                        "parentName" => "Suppliers"
-                      ],
-      */
       'orderColumn' => 1,
       'orderDirection' => 'ASC',
       'tagColumn' => 1,
@@ -62,32 +55,31 @@ class ERPCustomerCommentLinesUtils
     return $list;
   }
 
-
-  public function getExcludedForm($params){
-    return ['customer'];
+  public function formatListByCustomerTypeOrdersData($type,$parent){;
+    $list=[
+      'id' => 'list'.$this->name.$type,
+      'route' => 'customercommentlinesordersdatalist',
+      'routeParams' => ["customerid" => $parent, "type"=>$type],
+      'orderColumn' => 1,
+      'orderDirection' => 'ASC',
+      'tagColumn' => 1,
+      'fields' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/CustomerCommentLinesOrdersData.json"),true),
+      'fieldButtons' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/CustomerCommentLinesOrdersDataFieldButtons.json"),true),
+      'topButtons' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/CustomerCommentLinesOrdersDataTopButtons.json"),true)
+    ];
+    return $list;
   }
 
+
+  public function getExcludedForm($params){
+    return ["customer","type"];
+  }
 
   public function getIncludedForm($params){
     $doctrine=$params["doctrine"];
     $user=$params["user"];
     $id=$params["id"];
-    return [];
-  }
 
-  public function formatList($user){
-    $list=[
-      'id' => 'list'.$this->name,
-      'route' => 'genericlist',
-      'routeParams' => ["module" => $this->module,
-                        "name" => $this->name],
-      'orderColumn' => 2,
-      'orderDirection' => 'ASC',
-      'tagColumn' => 2,
-      'fields' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/".$this->name.".json"),true),
-      'fieldButtons' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/".$this->name."FieldButtons.json"),true),
-      'topButtons' => json_decode(file_get_contents (dirname(__FILE__)."/../Lists/".$this->name."TopButtons.json"),true)
-    ];
-    return $list;
+    return [];
   }
 }
