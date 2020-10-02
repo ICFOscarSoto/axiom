7429ec75df2e11e2dc92e0a5f025501fb97622b4
david.martinez@aplicode.com <O1vkj7w1O8>
2020-10-02 09:21:10 +0200

    fixed issues

--- src/Repository/Modules/AERP/Entity/AERPExpirationsRepository.php
+++ src/Repository/Modules/AERP/Entity/AERPExpirationsRepository.php
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 <?php
-
 namespace App\Repository\Modules\AERP\Entity;
 
 use App\Modules\AERP\Entity\AERPExpirations;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
-/**
- * @method ERPCalls|null find($id, $lockMode = null, $lockVersion = null)
- * @method ERPCalls|null findOneBy(array $criteria, array $orderBy = null)
- * @method ERPCalls[]    findAll()
- * @method ERPCalls[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
 class AERPExpirationsRepository extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
@@ -20,8 +12,4 @@ class AERPExpirationsRepository extends ServiceEntityRepository
         parent::__construct($registry, AERPExpirations::class);
     }
 
-
 }
-=======
-            ->getResult()
->>>>>>> 0c5fa59ca96f57398f8bea5da882c3c04878d7c7
