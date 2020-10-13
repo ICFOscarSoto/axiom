b269deac85050f378d519d0b975947d6d32f615a
david.martinez@aplicode.com <O1vkj7w1O8>
2020-10-13 09:55:45 +0200

    added barcode search to products

--- src/Modules/ERP/Controller/ERPProductsController.php
+++ src/Modules/ERP/Controller/ERPProductsController.php
@@ -64,6 +64,7 @@ class ERPProductsController extends Controller
   				'breadcrumb' =>  $menurepository->formatBreadcrumb($request->get('_route')),
   				'userData' => $userdata,
   				'lists' => $templateLists,
+					'include_pre_list_templates' => ['@ERP/product_barcode_search.html.twig'],
 					'include_post_templates' => ['@ERP/categoriesmap.html.twig','@ERP/productlistcategories.html.twig'],
 					'include_footer' => [["type"=>"css", "path"=>"/js/datetimepicker/bootstrap-datetimepicker.min.css"],
 															 ["type"=>"js",  "path"=>"/js/datetimepicker/bootstrap-datetimepicker.min.js"],
