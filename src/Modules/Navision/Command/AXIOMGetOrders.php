d95e7bf573ea29d3757f0722e5874d5dabd6093c
Oscar Soto <oscar.soto@ferricam.com>
2020-12-02 12:41:26 +0100

    fixed issues

--- src/Modules/Navision/Command/AXIOMGetOrders.php
+++ src/Modules/Navision/Command/AXIOMGetOrders.php
@@ -212,8 +212,9 @@ class AXIOMGetOrders extends ContainerAwareCommand
 
 
 
-        foreach($orders_id as $order_id){
-          $order=$repositorySalesOrders->findOneBy(["id"=>$order_id]);
+        //foreach($orders_id as $order_id){
+        //  $order=$repositorySalesOrders->findOneBy(["id"=>$order_id]);
+        foreach($orders as $order){
           if (strncmp($order->getCode(), "20PV", 4) === 0) $devolucion=0;
           else $devolucion=1;
           $output->writeln("Insertando el pedido: ".$order->getCode());
