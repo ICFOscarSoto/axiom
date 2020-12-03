2cb8a29825881559a90775b3475b240cc13c055f
Juanjo Roncero <juanjo.roncero@ferricam.com>
2020-12-03 12:00:44 +0100

    fixed issues

--- src/Modules/Navision/Command/AXIOMGetBudgets.php
+++ src/Modules/Navision/Command/AXIOMGetBudgets.php
@@ -89,7 +89,9 @@ class AXIOMGetBudgets extends ContainerAwareCommand
     //$orders=$repositoryPurchasesOrders->findAll();
     $budgets_id=$repositorySalesBudgets->findNews();
 
+    dump($budgets_id);
     foreach($budgets_id as $budget_id){
+      dump($budget_id);
       $budget=$repositorySalesBudgets->findOneBy(["id"=>$budget_id]);
       if($budget->getCode()!="20OFV11510") continue;
 
