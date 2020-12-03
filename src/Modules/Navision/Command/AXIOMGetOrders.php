32920b1224ae4ee766a6d03bf9abfbb4d6f092e2
Oscar Soto <oscar.soto@ferricam.com>
2020-12-03 07:59:44 +0100

    fixed issues

--- src/Modules/Navision/Command/AXIOMGetOrders.php
+++ src/Modules/Navision/Command/AXIOMGetOrders.php
@@ -296,7 +296,7 @@ class AXIOMGetOrders extends ContainerAwareCommand
 
 
         //$output->writeln(json_encode($orderJson,JSON_UNESCAPED_UNICODE));
-        $result=file_get_contents('http://192.168.1.250:9000/navisionExport/axiom/do-NAVISION-createSalesOrders.php?json='.urlencode(json_encode($orderJson,JSON_UNESCAPED_UNICODE)));
+        $result=file_get_contents('http://192.168.1.250:9000/navisionExport/axiom/do-NAVISION-createSalesOrders.php?json='.urlencode(json_encode($orderJson)));
 
       }
     //------   Critical Section END   ------
