430d6deac3fd65e4e4193bbb475d5035195aa7c3
Oscar Soto <oscar.soto@ferricam.com>
2021-01-11 12:52:05 +0100

    fixed issues

--- src/Modules/Navision/Command/NavisionGetProducts.php
+++ src/Modules/Navision/Command/NavisionGetProducts.php
@@ -1382,11 +1382,17 @@ public function exportNames(InputInterface $input, OutputInterface $output){
   //$products=$repositoryProducts->findAll();
   //foreach ($products as $product){
   $product=$repositoryProducts->findOneBy(["code"=>'55S77']);
+  $item=[];
+  $array_products=[];
   $item=[
     "code"=>$product->getCode(),
     "Description"=>substr($product->getName(),0,30),
     "Description 2"=>substr($product->getName(),30,30)];
   $output->writeln('Actualizando '.urlencode($item));
+
+  $array_products=$item;
+  $json = json_encode($array_products);
+
   $json=file_get_contents($this->url.'navisionExport/axiom/do-NAVISION-updateNames.php?from='.urlencode($item));
   //}
 }
