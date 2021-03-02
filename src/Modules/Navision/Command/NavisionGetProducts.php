67613e3577a8fc5f800dc8fccee9da135fd75107
OscarSotoICF <oscar.soto@ferricam.com>
2021-03-02 08:51:32 +0100

    findby 192119009

--- src/Modules/Navision/Command/NavisionGetProducts.php
+++ src/Modules/Navision/Command/NavisionGetProducts.php
@@ -535,7 +535,7 @@ public function groupPrices(InputInterface $input, OutputInterface $output){
 
 public function updateProducts(InputInterface $input, OutputInterface $output){
   $repository=$this->doctrine->getRepository(ERPProducts::class);
-  $products=$repository->findBy(['shoppingPrice'=>0, 'active'=>1]);
+  $products=$repository->findBy(['shoppingPrice'=>0, 'active'=>1, 'code'=>192119009]);
   foreach ($products as $product){
     $output->writeln("Cambiando el producto ".$product->getCode());
     $json=file_get_contents($this->url.'navisionExport/axiom/do-NAVISION-getProduct.php?product='.$product->getCode());
