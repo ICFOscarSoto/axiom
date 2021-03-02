627162cde866926954f36307ef58bf8aa2393023
OscarSotoICF <oscar.soto@ferricam.com>
2021-03-02 08:56:51 +0100

    change merge -> persist

--- src/Modules/Navision/Command/NavisionGetProducts.php
+++ src/Modules/Navision/Command/NavisionGetProducts.php
@@ -589,10 +589,10 @@ public function updateProducts(InputInterface $input, OutputInterface $output){
     $productPrices=$repositoryProductPrices->findBy(['product'=>$product->getId()]);
     foreach ($productPrices as $productPrice){
       $productPrice=$repositoryProductPrices->findOneBy(['id'=>$productPrice]);
-      $this->doctrine->getManager()->merge($productPrice);
+      $this->doctrine->getManager()->persist($productPrice);
       $this->doctrine->getManager()->flush();
     }
-    $this->doctrine->getManager()->merge($product);
+    $this->doctrine->getManager()->persist($product);
     $this->doctrine->getManager()->flush();
     $this->doctrine->getManager()->clear();
   }
