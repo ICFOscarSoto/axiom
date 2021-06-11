8dfdbb120d35eeb7aad07e9261707b5e324d6a73
Juanjo Roncero <juanjo.roncero@ferricam.com>
2021-06-11 11:01:59 +0200

    fixed issues

--- src/Modules/ERP/Command/InventoryRemember.php
+++ src/Modules/ERP/Command/InventoryRemember.php
@@ -71,7 +71,7 @@ class InventoryRemember extends ContainerAwareCommand
             $worker=$workersRepository->findOneBy(["user"=>$agent, "active"=>1, "deleted"=>0]);
             if($worker){
                 //Check if worker is working now
-               if(!$workersRepository->isWorking($worker)){
+                if(!$workersRepository->isWorking($worker)){
                   $output->writeln('   - El trabajador no esta trabajando ahora');
                 }
 
@@ -84,15 +84,15 @@ class InventoryRemember extends ContainerAwareCommand
                   $cont=1;
                   foreach ($inventory as $reference)
                   {
-                    /* if($cont<9)*/  $msg_products1=$msg_products1."**".$reference."**\n";
-                    //  else if($cont<17) $msg_products2=$msg_products2."**".$reference."**\n";
-                    //  else $msg_products3=$msg_products3."**".$reference."**\n";
+                      if($cont<9) $msg_products1=$msg_products1."**".$reference."**\n";
+                      else if($cont<17) $msg_products2=$msg_products2."**".$reference."**\n";
+                      else $msg_products3=$msg_products3."**".$reference."**\n";
                       $cont++;
                   }
 
                   $cont--;
-                /*  if($cont<9)*/ file_get_contents('https://icfbot.ferreteriacampollano.com/message.php?channel='.$channel_agent.'&msg='.urlencode($msg_title."\n\n".$msg_products1));
-                /*
+                  if($cont<9) file_get_contents('https://icfbot.ferreteriacampollano.com/message.php?channel='.$channel_agent.'&msg='.urlencode($msg_title."\n\n".$msg_products));
+
                   else if($cont<17){
                     file_get_contents('https://icfbot.ferreteriacampollano.com/message.php?channel='.$channel_agent.'&msg='.urlencode($msg_title."\n\n".$msg_products1));
                     file_get_contents('https://icfbot.ferreteriacampollano.com/message.php?channel='.$channel_agent.'&msg='.urlencode($msg_products2));
@@ -108,7 +108,7 @@ class InventoryRemember extends ContainerAwareCommand
                     $msg="Exceso de productos para inventariar en almacén ".$store["name"];
                     file_get_contents('https://icfbot.ferreteriacampollano.com/message.php?channel=822001670623199262&msg='.urlencode($msg));
                   }
-                  */
+
                   $output->writeln('   - Notificación recordatorio enviada al gestor de '.$store["name"]);
                 }
             }
