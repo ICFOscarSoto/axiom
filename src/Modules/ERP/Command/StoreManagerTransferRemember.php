895ce727472d4e0f83ef28bd033ab41397daace7
David Martinez <david.martinez@aplicode.com>
2021-07-12 08:31:15 +0200

    Added command to remember transfers

---
+++ src/Modules/ERP/Command/StoreManagerTransferRemember.php
@@ -0,0 +1,87 @@
+<?php
+namespace App\Modules\ERP\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\Modules\Globale\Entity\GlobaleUsers;
+use App\Modules\ERP\Entity\ERPStoreLocations;
+use App\Modules\ERP\Entity\ERPProductsVariants;
+use App\Modules\ERP\Entity\ERPStocks;
+use App\Modules\ERP\Entity\ERPStockHistory;
+use App\Modules\ERP\Entity\ERPInfoStocks;
+use App\Modules\ERP\Entity\ERPSalesTickets;
+use App\Modules\ERP\Entity\ERPStores;
+use App\Modules\ERP\Entity\ERPProducts;
+use App\Modules\ERP\Entity\ERPStoresManagers;
+use App\Modules\Globale\Entity\GlobaleCompanies;
+
+
+
+class StoreManagerTransferRemember extends ContainerAwareCommand
+{
+  protected function configure(){
+        $this
+            ->setName('ERP:managerRemember')
+            ->setDescription('Recordatorio de transferencias de managers')
+            ->addArgument('var_store', InputArgument::REQUIRED, '¿Almacen sobre el que informar?')
+        ;
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+        $doctrine = $this->getContainer()->get('doctrine');
+        $entityManager = $doctrine->getManager();
+        $var_store = $input->getArgument('var_store');
+        $usersRepository=$doctrine->getRepository(GlobaleUsers::class);
+        $locationsRepository=$doctrine->getRepository(ERPStoreLocations::class);
+        $productsVariantsRepository=$doctrine->getRepository(ERPProductsVariants::class);
+        $stocksRepository=$doctrine->getRepository(ERPStocks::class);
+        $stockHistoryRepository=$doctrine->getRepository(ERPStockHistory::class);
+        $infoStocksRepository=$doctrine->getRepository(ERPInfoStocks::class);
+        $storesRepository=$doctrine->getRepository(ERPStores::class);
+        $productsRepository=$doctrine->getRepository(ERPProducts::class);
+        $companiesrepository=$doctrine->getRepository(GlobaleCompanies::class);
+        $managerepository=$doctrine->getRepository(ERPStoresManagers::class);
+
+        $output->writeln('');
+        $output->writeln('ENVIANDO TRASPASOS DE GESTORES');
+        $output->writeln('===================================================');
+        //$user=$usersRepository->findOneBy(["email"=>$username, "deleted"=>0]);
+        //if(!$user) die('- El usuario especificado no existe');
+        //$company=$user->getCompany();
+
+        $store=$storesRepository->findOneBy(["code"=>$var_store,"deleted"=>0]);
+        if(!$store) {
+          die(' - Almacén no encontrado');
+        }
+        $manager=$managerepository->findOneBy(["name"=>"BABCOCK"]);
+        if(!$store) {
+          die(' - Gestor no encontrado');
+        }
+        $info_stocks=$infoStocksRepository->findBy(["store"=>$store, "active"=>1, "deleted"=>0]);
+        foreach($info_stocks as $infostock){
+            $location=$locationsRepository->findOneBy(["name"=>$store->getCode(), "active"=>1, "deleted"=>0]);
+            if(!$location) {
+              die(' - Ubicación no encontrada');
+            }
+            $stock=$stocksRepository->findOneBy(["product"=>$infostock->getProduct(), "productvariant"=>$infostock->getProductvariant(), "storelocation"=>$location, "active"=>1, "deleted"=>0]);
+            if(!$location) {
+                $output->writeln(' - Stock no encontrado');
+                continue;
+            }
+            if($infostock->getMinimumQuantity()>=$stock->getQuantity()){
+              //Inform to discotd channel
+              if($manager->getDiscordchannel()!=null){
+                $channel=$manager->getDiscordchannel();
+                $msg="Ref: **".$infostock->getProduct()->getCode()."** - ".$infostock->getProduct()->getName()." realizar traspaso a **".$store->getName()."** - Cantidad: **".($infostock->getMaximunQuantity()-$stock->getQuantity()." unidades.**");
+                file_get_contents('https://icfbot.ferreteriacampollano.com/message.php?channel='.$channel.'&msg='.urlencode($msg));
+              }
+            }
+        }
+
+  }
+}
+?>
