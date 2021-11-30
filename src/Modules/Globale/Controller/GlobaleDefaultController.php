39c38a49df9915bc2bec92140b3306bfd22a3d93
David Martinez <david.martinez@aplicode.com>
2021-11-30 20:13:32 +0100

    fixed inputs issues

--- src/Modules/Globale/Controller/GlobaleDefaultController.php
+++ src/Modules/Globale/Controller/GlobaleDefaultController.php
@@ -192,9 +192,6 @@ class GlobaleDefaultController extends Controller
        }
        $listUtils=new GlobaleListUtils();
 
-
-
-       /*
        if($json!="")
         $listFields=json_decode(file_get_contents (dirname(__FILE__)."/../../".$module."/Lists/".$json.".json"),true);
        else{
@@ -207,11 +204,7 @@ class GlobaleDefaultController extends Controller
             $listFields=json_decode(file_get_contents (dirname(__FILE__)."/../../".$module."/Lists/".$name.".json"),true);
          }
        }
-       if(property_exists($class, "user") && !in_array("ROLE_GLOBAL", $user->getRoles()) && !in_array("ROLE_SUPERADMIN", $user->getRoles()) && !in_array("ROLE_ADMIN", $user->getRoles())) $return=$listUtils->getRecords($user,$repository,$request,$manager,$listFields, $class, $filter);
-        else if(property_exists($class, "company")) $return=$listUtils->getRecords($user,$repository,$request,$manager,$listFields, $class, $filter);
-          else $return=$listUtils->getRecords($user,$repository,$request,$manager,$listFields, $class, count($filter)>1?[$filter[1]]:[]);
-       return new JsonResponse($return);*/
-       $listFields=json_decode(file_get_contents (dirname(__FILE__)."/../../".$module."/Lists/".($json!=""?$json:$name).".json"),true);
+
        if(property_exists($class, "user") && !in_array("ROLE_GLOBAL", $user->getRoles()) && !in_array("ROLE_SUPERADMIN", $user->getRoles()) && !in_array("ROLE_ADMIN", $user->getRoles())) $return=$listUtils->getRecords($user,$repository,$request,$manager,$listFields, $class, $filter);
         else if(property_exists($class, "company")) $return=$listUtils->getRecords($user,$repository,$request,$manager,$listFields, $class, $filter);
           else $return=$listUtils->getRecords($user,$repository,$request,$manager,$listFields, $class, count($filter)>1?[$filter[1]]:[]);
