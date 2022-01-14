dd5c370276cb8dee1151f86132843297d8234d31
David Martinez <david.martinez@aplicode.com>
2022-01-14 10:01:39 +0100

    Removed library PhpSpreadsheet

--- src/Modules/Globale/Helpers/PhpOffice/PhpSpreadsheet/Exception.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Modules\Globale\Helpers\PhpOffice\PhpSpreadsheet;
-
-class Exception extends \Exception
-{
-}
--- src/Modules/Globale/Helpers/PhpOffice/PhpSpreadsheet/Writer/Exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Modules\Globale\Helpers\PhpOffice\PhpSpreadsheet\Writer;
-
-use PhpOffice\PhpSpreadsheet\Exception as PhpSpreadsheetException;
-
-class Exception extends PhpSpreadsheetException
-{
-}
