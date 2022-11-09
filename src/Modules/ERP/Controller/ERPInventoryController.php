f9c80780e203e75b550b242930e1abc8eace475a
OscarSotoICF <oscar.soto@ferricam.com>
2022-11-09 12:24:17 +0100

    added listInventory

--- src/Modules/ERP/Controller/ERPInventoryController.php
+++ src/Modules/ERP/Controller/ERPInventoryController.php
@@ -14,6 +14,8 @@ use App\Modules\Globale\Entity\GlobaleUsers;
 use App\Modules\Globale\Utils\GlobaleEntityUtils;
 use App\Modules\Globale\Utils\GlobaleListUtils;
 use App\Modules\Globale\Utils\GlobaleFormUtils;
+use App\Modules\ERP\Utils\ERPInventoryUtils;
+use App\Modules\ERP\Utils\ERPInventoryLinesUtils;
 use App\Modules\ERP\Entity\ERPInventory;
 use App\Modules\ERP\Entity\ERPInventoryLines;
 use App\Modules\ERP\Entity\ERPInventoryLocation;
@@ -21,10 +23,12 @@ use App\Modules\ERP\Entity\ERPStores;
 use App\Modules\ERP\Entity\ERPStoreLocations;
 use App\Modules\ERP\Entity\ERPStocks;
 use App\Modules\ERP\Entity\ERPStocksHistory;
+use App\Modules\Security\Utils\SecurityUtils;
 
 class ERPInventoryController extends Controller
 {
 	private $class=ERPInventory::class;
+	private $module='ERP';
 		private $utilsClass=ERPInventoryUtils::class;
 
 	/**
@@ -218,4 +222,84 @@ class ERPInventoryController extends Controller
 		$return['dateupd'] = ($oinventorylines->getDateupd()!=null?date_format($oinventorylines->getDateupd(), "Y/m/d H:i:s"):'');
 		return $return;
 	}
+
+	/**
+	 * @Route("/{_locale}/ERP/inventory", name="inventory")
+	 */
+	public function index(RouterInterface $router,Request $request){
+		$this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+		if(!SecurityUtils::checkRoutePermissions($this->module,$request->get('_route'),$this->getUser(), $this->getDoctrine())) return $this->redirect($this->generateUrl('unauthorized'));
+
+		$userdata=$this->getUser()->getTemplateData($this, $this->getDoctrine());
+		$locale = $request->getLocale();
+		$this->router = $router;
+		$menurepository=$this->getDoctrine()->getRepository(GlobaleMenuOptions::class);
+		$utils = new $this->utilsClass();
+		$templateLists[]=$utils->formatList($this->getUser());
+		$formUtils=new GlobaleFormUtils();
+		$formUtils->initialize($this->getUser(), new $this->class(), dirname(__FILE__)."/../Forms/Inventory.json", $request, $this, $this->getDoctrine());
+		$templateForms[]=$formUtils->formatForm('stores', true, null, $this->class);
+		if ($this->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+			return $this->render('@Globale/genericlist.html.twig', [
+				'controllerName' => 'inventoryController',
+				'interfaceName' => 'Inventory',
+				'optionSelected' => $request->attributes->get('_route'),
+				'menuOptions' =>  $menurepository->formatOptions($userdata),
+				'breadcrumb' =>  $menurepository->formatBreadcrumb($request->get('_route')),
+				'userData' => $userdata,
+				'lists' => $templateLists,
+				'forms' => $templateForms
+			]);
+			}
+			return new RedirectResponse($this->router->generate('app_login'));
+		}
+
+
+	/**
+   * @Route("/api/linesInventory/{id}/list", name="linesInventoryList")
+   */
+  public function indexlist($id,RouterInterface $router,Request $request){
+    $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+    $user = $this->getUser();
+    $locale = $request->getLocale();
+    $this->router = $router;
+    $manager = $this->getDoctrine()->getManager();
+    $class=ERPInventoryLines::class;
+    $repository = $manager->getRepository($class);
+    $listUtils=new GlobaleListUtils();
+    $listFields=json_decode(file_get_contents (dirname(__FILE__)."/../Lists/InventoryLines.json"),true);
+    $return=$listUtils->getRecords($user,$repository,$request,$manager,$listFields, $class, [["type"=>"and","column"=>"inventory", "value"=>$id]]);
+    return new JsonResponse($return);
+  }
+
+
+
+	/**
+	 * @Route("/{_locale}/ERP/inventory/form/{id}", name="formERPInventory", defaults={"id"=0})
+	 */
+	public function formERPInventory($id,Request $request){
+		$this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+		if(!SecurityUtils::checkRoutePermissions($this->module,$request->get('_route'),$this->getUser(), $this->getDoctrine())) return $this->redirect($this->generateUrl('unauthorized'));
+		$userdata=$this->getUser()->getTemplateData($this, $this->getDoctrine());
+		$locale = $request->getLocale();
+		$menurepository=$this->getDoctrine()->getRepository(GlobaleMenuOptions::class);
+		$repository=$this->getDoctrine()->getRepository($this->class);
+		$obj = $repository->findOneBy(['id'=>$id, 'company'=>$this->getUser()->getCompany(), 'deleted'=>0]);
+		$new_breadcrumb=["rute"=>null, "name"=>$id?"Editar":"Nuevo", "icon"=>$id?"fa fa-edit":"fa fa-plus"];
+		$breadcrumb=$menurepository->formatBreadcrumb('inventory');
+		array_push($breadcrumb,$new_breadcrumb);
+		$utils = new ERPInventoryLinesUtils();
+		$templateLists[]=$utils->formatListByInventory($id);
+		return $this->render('@ERP/inventoryLinesList.html.twig', [
+			'controllerName' => 'inventoryController',
+			'interfaceName' => 'Inventory',
+			'optionSelected' => $request->attributes->get('_route'),
+			'menuOptions' =>  $menurepository->formatOptions($userdata),
+			'breadcrumb' =>  $menurepository->formatBreadcrumb($request->get('_route')),
+			'userData' => $userdata,
+			'inventory' => $obj,
+			'linesInventory' => $templateLists
+		]);
+	 }
+
 }
