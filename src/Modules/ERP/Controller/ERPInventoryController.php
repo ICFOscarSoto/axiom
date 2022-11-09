73b4bec6ca9e985066e73e2eb15df64891b442cc
antoniobleda <antonio.bleda@ferreteriacampollano.com>
2022-11-09 12:50:14 +0100

    Inventory list locations

--- src/Modules/ERP/Controller/ERPInventoryController.php
+++ src/Modules/ERP/Controller/ERPInventoryController.php
@@ -49,6 +49,7 @@ class ERPInventoryController extends Controller
 		// Repositorios ------------------------------------
 		$erpInventoryRepository			= $this->getDoctrine()->getRepository(ERPInventory::class);
 		$erpInventoryLinesRepository= $this->getDoctrine()->getRepository(ERPInventoryLines::class);
+		$erpInventoryLocationRepository= $this->getDoctrine()->getRepository(ERPInventoryLocation::class);
 		$erpStoresRepository				= $this->getDoctrine()->getRepository(ERPStores::class);
 		$erpStoreLocationsRepository= $this->getDoctrine()->getRepository(ERPStoreLocations::class);
 		$globaleCompaniesRepository	= $this->getDoctrine()->getRepository(GlobaleCompanies::class);
@@ -148,8 +149,9 @@ class ERPInventoryController extends Controller
 						foreach ($oinventorylines as $key => $value) {
 							array_push($return['data'],$this->getInventoryLinesResult($value));
 						}
-						$return['text'] 	= "Inventario - Ubicación - Todos los productos";
+						$return['text'] 	= "Inventario - Todos los productos";
 					}else{
+					// De una ubicación
 						$ostorelocation			= $erpStoreLocationsRepository->findOneBy(["id"=>$location_id, "deleted"=>0]);
 						if ($ostorelocation){
 							$oinventorylines		= $erpInventoryLinesRepository->findBy(["inventory"=>$oinventory, "location"=>$ostorelocation, "active"=>1, "deleted"=>0],['dateadd' => 'ASC']);
@@ -158,7 +160,7 @@ class ERPInventoryController extends Controller
 							foreach ($oinventorylines as $key => $value) {
 								array_push($return['data'],$this->getInventoryLinesResult($value));
 							}
-							$return['text'] 	= "Inventario - Ubicación - Productos de la ubicación: ".$ostorelocation->getName();
+							$return['text'] 	= "Inventario - Productos de la ubicación: ".$ostorelocation->getName();
 						}else
 							$return = ["result"=>-1, "text"=>'Inventario - Ubicación - Identificador no válido'];
 					}
@@ -166,6 +168,45 @@ class ERPInventoryController extends Controller
 					$return = ["result"=>-1, "text"=>'Inventario - Identificador no válido'];
 				break;
 
+			// locations -> Para el identificador de inventario pasado como argumento
+			//					obtiene las ubicaciones ya procesadas
+			case 'locations':
+				// Parámetros adicionales
+				$location_id 	= $request->request->get('location_id');
+				$oinventory		= $erpInventoryRepository->findOneBy(["id"=>$id, "deleted"=>0]);
+				if ($oinventory!=null){
+					// Todas
+					if ($location_id==null){
+						$oinventorylocation		= $erpInventoryLocationRepository->findBy(["inventory"=>$oinventory, "active"=>1, "deleted"=>0],['dateadd' => 'ASC']);
+						$return['result'] = 1;
+						$return['data'] 	= [];
+						foreach ($oinventorylocation as $key => $value) {
+							array_push($return['data'],$this->getInventoryLocationResult($value));
+						}
+						$return['text'] 	= "Inventario - Ubicación - Todas las ubicaciones";
+					}else{
+					// Comprueba si la ubicación es válida para este inventario sino -1 y mensaje
+					// Si es válida pero no esta la base de datos de inventarios/ubicaciones se pone 1 pero data vacio
+					// Si existe se devuelve en data
+						$ostorelocation			= $erpStoreLocationsRepository->findOneBy(["id"=>$location_id, "deleted"=>0]);
+						if ($ostorelocation){
+							// Comprobar si es una ubicación válida para este inventario
+							if ($ostorelocation->getStore()->getId()==$id){
+								$oinventorylocation		= $erpInventoryLocationRepository->findOneBy(["inventory"=>$oinventory, "location"=>$ostorelocation, "active"=>1, "deleted"=>0],['dateadd' => 'ASC']);
+								$return['result'] = 1;
+								$return['data'] 	= [];
+								if ($oinventorylocation!= null)
+									$return['data'] = $this->getInventoryLocationResult($oinventorylocation);
+								$return['text'] 	= "Inventario - Ubicación: ".$ostorelocation->getName();
+							}else
+								$return = ["result"=>-1, "text"=>'Inventario - Ubicación no válida para el inventario'];
+						}else
+							$return = ["result"=>-1, "text"=>'Inventario - Ubicación - Identificador no válido'];
+					}
+				}else
+					$return = ["result"=>-1, "text"=>'Inventario - Identificador no válido'];
+				break;
+				
 			// Acción no válida
 			default:
 				$return = ["result"=>-1, "text"=>'Inventario - Acción no válida'];
@@ -216,6 +257,24 @@ class ERPInventoryController extends Controller
 		$return['deleted'] = $oinventorylines->getDeleted();
 		$return['dateadd'] = ($oinventorylines->getDateadd()!=null?date_format($oinventorylines->getDateadd(), "Y/m/d H:i:s"):'');
 		$return['dateupd'] = ($oinventorylines->getDateupd()!=null?date_format($oinventorylines->getDateupd(), "Y/m/d H:i:s"):'');
+		return $return;
+	}
+
+	private function getInventoryLocationResult(ERPInventoryLocation $oinventorylocation){
+		$return = [];
+		$return['id'] = $oinventorylocation->getId();
+		$return['inventory_id'] = $oinventorylocation->getInventory()->getId();
+		$return['inventory_name'] = $oinventorylocation->getInventory()->getObservation();
+		$return['location_id'] = $oinventorylocation->getLocation()->getId();
+		$return['location_name'] = $oinventorylocation->getLocation()->getName();
+		$return['author_id'] = $oinventorylocation->getAuthor()->getId();
+		$return['author_name'] = $oinventorylocation->getAuthor()->getName().' '.$oinventorylocation->getAuthor()->getLastname();
+		$return['datebegin'] = ($oinventorylocation->getDatebegin()!=null?date_format($oinventorylocation->getDatebegin(), "Y/m/d H:i:s"):'');
+		$return['dateend'] = ($oinventorylocation->getDateend()!=null?date_format($oinventorylocation->getDateend(), "Y/m/d H:i:s"):'');
+		$return['active'] = $oinventorylocation->getActive();
+		$return['deleted'] = $oinventorylocation->getDeleted();
+		$return['dateadd'] = ($oinventorylocation->getDateadd()!=null?date_format($oinventorylocation->getDateadd(), "Y/m/d H:i:s"):'');
+		$return['dateupd'] = ($oinventorylocation->getDateupd()!=null?date_format($oinventorylocation->getDateupd(), "Y/m/d H:i:s"):'');
 		return $return;
 	}
 }
