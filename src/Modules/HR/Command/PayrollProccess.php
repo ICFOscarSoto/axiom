aef029672cc75b86247f062ac82b67f640c8eb3c
David Martinez <david.martinez@aplicode.com>
2023-01-31 19:29:42 +0100

    fixed issues

--- src/Modules/HR/Command/PayrollProccess.php
+++ src/Modules/HR/Command/PayrollProccess.php
@@ -64,8 +64,7 @@ class PayrollProccess extends ContainerAwareCommand
     $dir = new \DirectoryIterator($tempDir);
     foreach ($dir as $fileinfo) {
       //Añadir formato corporativo
-      $ocrDir.basename($fileinfo->getFilename(), '.pdf')."_%04d.pdf
-      $result=shell_exec("pdftk \"".$tempDir.$fileinfo->getFilename()."\" stamp /home/operador/nominas/plantilla_nominas.pdf output \"".$tempDir.basename($fileinfo->getFilename(), '.pdf')."_%04format.pdf."\"");
+      $result=shell_exec("pdftk \"".$tempDir.$fileinfo->getFilename()."\" stamp /home/operador/nominas/plantilla_nominas.pdf output \"".$ocrDir.basename($fileinfo->getFilename(), '.pdf')."_%04d.pdf."\"");
       unlink($tempDir.$fileinfo->getFilename());
       //Firmar documentos
       //pdftk file63d91db71abef_0046.pdf stamp /home/operador/nominas/plantilla_nominas.pdf output format_file63d91db71abef_0046.pdf
